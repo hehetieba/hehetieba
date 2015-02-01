fb822ed423f2f5b605cc7fbe540b3915eed0de16
YaoZihuan <yaozihuan@163.com>
2015-02-01 23:23:14 +0800

    111

--- hehetieba/src/hehetieba/action/UserAction.java
+++ hehetieba/src/hehetieba/action/UserAction.java
@@ -5,5 +5,14 @@ import hehetieba.service.IUserService;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UserAction extends ActionSupport {
-	IUserService iUserService;
+	private IUserService iUserService;
+
+	public IUserService getiUserService() {
+		return iUserService;
+	}
+
+	public void setiUserService(IUserService iUserService) {
+		this.iUserService = iUserService;
+	}
+	
 }
