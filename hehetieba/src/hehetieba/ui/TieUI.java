747923ea7621df25a35ec2acccb740b904a536e8
YaoZihuan <yaozihuan@163.com>
2015-03-16 22:54:43 +0800

    2015 3 16 22:54

---
+++ hehetieba/src/hehetieba/ui/TieUI.java
@@ -0,0 +1,11 @@
+package hehetieba.ui;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TieUI extends ActionSupport {
+//	@Override
+//	public String execute() {
+//		
+//		return null;
+//	}
+}
